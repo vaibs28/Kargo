34cc3cad449468e2b11ff40146e8c17a7203ef37
Vaibhav <vsahay28@gmail.com>
2020-03-15 02:41:35 -0400

    fix for failing testcase bar foo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 if(len(sys.argv) != 3):
 	print("Expecting 2 strings as command line arguments")
 	exit()
-	
+
 #if 3 command line args are passed
 str1 = sys.argv[1]
 str2 = sys.argv[2]
@@ -16,14 +16,9 @@ if(len(str1)!=len(str2)):
 dict = {}
 for i in range(len(str1)):
 	if str1[i] in dict:
-		if(dict[str1[i]]!=str2[i]):
+		if(dict[str1[i]]!=str2[i]):	
 			print("false")
 			exit()
 	else:
-		if str2[i] in dict.values():
-			print("false")
-			exit()
-		else:
-			dict[str1[i]] = str2[i]
+		dict[str1[i]] = str2[i]
 print("true")
-
